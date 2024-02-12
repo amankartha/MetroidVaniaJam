3e99d49727a798fad8f3bb69896e85a4ac07cb23
Aman Kartha <amankartha1998@gmail.com>
2024-02-12 12:24:35 -0500

    Added TMP and Scene inspector thing

---
+++ Assets/Game/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using MoreMountains.Tools;
+using UnityEngine;
+
+public class GameManager : MMPersistentSingleton<GameManager>
+{
+    #region VARIABLES
+    
+    
+    //main player stuff
+    public GameObject goMainPlayer;
+    public Transform tMainPlayer;
+    
+    #endregion
+    
+    #region UNITYMETHODS
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+    #endregion
+  
+}
