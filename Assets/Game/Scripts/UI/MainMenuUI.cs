0ea727d002cf49bea3d78b4fbf0099da8540b98e
Aman Kartha <amankartha1998@gmail.com>
2024-02-17 16:26:49 -0500

    started on scene stuff

--- Assets/Game/Scripts/UI/MainMenuUI.cs
+++ Assets/Game/Scripts/UI/MainMenuUI.cs
@@ -1,20 +1,72 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Eflatun.SceneReference;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
-    public SceneReference sMainGame;
+   
+    [Header("Main Menu Objects")]
+    [SerializeField] private GameObject _loadingBarGO;
+    [SerializeField] private Image _loadingBarImage;
+    [SerializeField] private GameObject[] _objectsToHide;
+
+    [Header("Scenes to load")] 
+    [SerializeField] private SceneReference _persistentGameplayScene;
+    [SerializeField] private SceneReference _levelScene;
+
+    private List<AsyncOperation> _scenesToLoad = new List<AsyncOperation>();
+    #region UNITYMETHODS
+
+    private void Awake()
+    {
+        _loadingBarGO.SetActive(false);
+    }
+
+    #endregion
+
 
     #region METHODS
 
-    public void LoadScene()
+    private void HideMenu()
     {
-        UTIL.LoadScene(sMainGame);
+        for (int i = 0; i < _objectsToHide.Length; i++)
+        {
+            _objectsToHide[i].SetActive(false);
+        }
     }
     
+    
+    public void StartGame()
+    {
+        HideMenu();
+        _loadingBarGO.SetActive(true);
+        
+        SceneManager.LoadSceneAsync(_persistentGameplayScene.BuildIndex);
+        SceneManager.LoadSceneAsync(_levelScene.BuildIndex,LoadSceneMode.Additive);
+
+        StartCoroutine(ProgressLoadingBar());
+    }
+
+    private IEnumerator ProgressLoadingBar()
+    {
+        float loadProgress = 0f;
+        for (int i = 0; i <_scenesToLoad.Count; i++)
+        {
+            while (!_scenesToLoad[i].isDone)
+            {
+                loadProgress += _scenesToLoad[i].progress;
+                _loadingBarImage.fillAmount = loadProgress / _scenesToLoad.Count;
+                yield return null;
+            }
+        }
+    }
+
+
+
 
     #endregion
 }
--- Assets/Game/Scripts/Util/UTIL.cs
+++ Assets/Game/Scripts/Util/UTIL.cs
@@ -6,8 +6,6 @@ using UnityEngine.SceneManagement;
 
 public static class UTIL 
 {
-
-    
     public static void LoadScene(SceneReference sceneReference)
     {
         SceneManager.LoadScene(sceneReference.BuildIndex);
