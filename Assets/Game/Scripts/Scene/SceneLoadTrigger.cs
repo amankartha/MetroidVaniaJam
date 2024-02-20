5e8d8a0505d1ba34c9845e86032091a07dc5ec9a
Aman Kartha <amankartha1998@gmail.com>
2024-02-19 21:28:03 -0500

    Scene load triggers

--- Assets/Game/Scripts/Scene/SceneLoadTrigger.cs
+++ Assets/Game/Scripts/Scene/SceneLoadTrigger.cs
@@ -1,10 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Eflatun.SceneReference;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoadTrigger : MonoBehaviour
 {
+    [SerializeField] private SceneReference[] _scenesToLoad;
+    [SerializeField] private SceneReference[] _scenesToUnload;
+
 
     #region UNITYMETHODS
 
@@ -27,6 +32,51 @@ public class SceneLoadTrigger : MonoBehaviour
             //load scene and unload
         }
     }
+
+    #endregion
+
+    #region Methods
+
+
+    private void LoadScenes()
+    {
+       
+        for (int i = 0; i < _scenesToLoad.Length; i++)
+        {
+            bool isSceneLoaded = false;
+            for (int j = 0; j < SceneManager.sceneCount; j++)
+            {
+                Scene loadedScene = SceneManager.GetSceneAt(j);
+                if (loadedScene.name == _scenesToLoad[i].Name)
+                {
+                    isSceneLoaded = true;
+                    break;
+                }
+            }
+
+            if (!isSceneLoaded)
+            {
+                SceneManager.LoadSceneAsync(_scenesToLoad[i].BuildIndex, LoadSceneMode.Additive);
+            }
+        }
+    }
+
+    private void UnloadScene()
+    {
+        for (int i = 0; i < _scenesToUnload.Length; i++)
+        {
+            for (int j = 0; j < SceneManager.sceneCount; j++)
+            {
+                Scene loadedScene = SceneManager.GetSceneAt(j);
+                if (loadedScene.name == _scenesToUnload[i].Name)
+                {
+                    SceneManager.UnloadSceneAsync(_scenesToUnload[i].BuildIndex);
+                }
+                    
+            }
+        }
+    }
+    
 
     #endregion
 }
