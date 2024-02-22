72fb044fa1a5228c569907911155374d2911cbc5
Aman Kartha <amankartha1998@gmail.com>
2024-02-21 23:43:55 -0500

    continued on input

--- Assets/Game/Scripts/Player/FiniteStateMachine/Player.cs
+++ Assets/Game/Scripts/Player/FiniteStateMachine/Player.cs
@@ -12,9 +12,15 @@ public class Player : MonoBehaviour
     public PlayerIdleState IdleState { get; private set; }
     public PlayerMoveState MoveState { get; private set; }
     public Animator Anim { get; private set; }
+    public PlayerInputHandler InputHandler { get; private set; }
+    public Rigidbody2D RB { get; private set; }
+    
+    public Vector2 CurrentVelocity { get; private set; }
 
     [SerializeField] 
     private PlayerData _playerData;
+
+    private Vector2 workspace;
 
     private void Awake()
     {
@@ -27,16 +33,26 @@ public class Player : MonoBehaviour
     private void Start()
     {
         Anim = GetComponent<Animator>();
+        InputHandler = GetComponent<PlayerInputHandler>();
+        RB = GetComponent<Rigidbody2D>();
         StateMachine.Initialize(IdleState);
     }
 
     private void Update()
     {
+        CurrentVelocity = RB.velocity;
         StateMachine.CurrentState.LogicUpdate();
     }
 
     private void FixedUpdate()
     {
         StateMachine.CurrentState.PhysicsUpdate();
+    }
+
+    public void SetVelocityX(float velocity)
+    {
+        workspace.Set(velocity, CurrentVelocity.y);
+        RB.velocity = workspace;
+        CurrentVelocity = workspace;
     }
 }
--- Assets/Game/Scripts/Player/PlayerStates/SubStates/PlayerMoveState.cs
+++ Assets/Game/Scripts/Player/PlayerStates/SubStates/PlayerMoveState.cs
@@ -7,4 +7,34 @@ public class PlayerMoveState : PlayerGroundedState
     public PlayerMoveState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
     {
     }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        if (input.x == 0f)
+        {
+            _stateMachine.ChangeState(_player.IdleState);
+        }
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+
+    public override void DoChecks()
+    {
+        base.DoChecks();
+    }
 }
