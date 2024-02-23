a669c437db880598c84e66cdf041476dcb751bef
Aman Kartha <amankartha1998@gmail.com>
2024-02-23 16:24:41 -0500

    double jump

--- Assets/Game/Scripts/Player/PlayerStates/SuperStates/PlayerGroundedState.cs
+++ Assets/Game/Scripts/Player/PlayerStates/SuperStates/PlayerGroundedState.cs
@@ -8,6 +8,7 @@ public class PlayerGroundedState : PlayerState
 
     protected int _xInput;
     private bool _jumpInput;
+    private bool _isGrounded;
     public PlayerGroundedState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
     {
         
@@ -35,6 +36,11 @@ public class PlayerGroundedState : PlayerState
            _player.InputHandler.UseJumpInput();
            _stateMachine.ChangeState(_player.JumpState);
        }
+       else if(!_isGrounded)
+       {
+           _player.JumpState.DecreaseAmountOfJumpsLeft();
+           _stateMachine.ChangeState(_player.InAirState);
+       }
     }
 
     public override void PhysicsUpdate()
@@ -45,5 +51,6 @@ public class PlayerGroundedState : PlayerState
     public override void DoChecks()
     {
         base.DoChecks();
+        _isGrounded = _player.CheckIfGrounded();
     }
 }
