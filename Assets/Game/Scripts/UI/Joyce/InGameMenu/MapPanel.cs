b1741ddc2fc140177c5fd1b86a105d66a8d983f0
Takoyyaki <yyinl695662@gmail.com>
2024-03-06 14:46:22 -0500

    basic toggle map zoom

--- Assets/Game/Scripts/UI/Joyce/InGameMenu/MapPanel.cs
+++ Assets/Game/Scripts/UI/Joyce/InGameMenu/MapPanel.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DG.Tweening;
-
+using TMPro;
+using UnityEngine.InputSystem;
 
 public class MapPanel : MonoBehaviour
 {
     [SerializeField] RectTransform mapRightPageRect;
     [SerializeField] RectTransform mapHolderRect;
+    [SerializeField] GameObject zoomTextHolder;
+    [SerializeField] TMP_Text zoomText;
 
     public Quaternion targetMapRightRotation;
     Quaternion originalMapRightRotation;
@@ -20,11 +24,23 @@ public class MapPanel : MonoBehaviour
 
     bool isFolded = true;
 
+    [SerializeField] Sprite roomMapLeftSprite;
+    [SerializeField] Sprite roomMapRightSprite;
+    [SerializeField] Sprite worldMapLeftSprite;
+    [SerializeField] Sprite worldMapRightSprite;
+    [SerializeField] Image mapLeftImage;
+    [SerializeField] Image mapRightImage;
+
+    bool isZoomedIn;
+
     private void Start()
     {
         originalMapRightRotation = mapRightPageRect.rotation;
         originalMapHolderRotation = mapHolderRect.rotation;
         originalMapHolderPosition = mapHolderRect.anchoredPosition;
+        mapLeftImage.sprite = worldMapLeftSprite;
+        mapRightImage.sprite = worldMapRightSprite;
+        zoomText.text = "Zoom In";
     }
 
     void OnEnable()
@@ -32,13 +48,22 @@ public class MapPanel : MonoBehaviour
         UnfoldMap();
     }
 
+    private void Update()
+    {
+        if (Keyboard.current.mKey.wasPressedThisFrame && !isFolded)
+        {
+            ToggleMapZoom();
+        }
+    }
+
     public void UnfoldMap()
     {
         if (isFolded)
         {
-            mapRightPageRect.DORotateQuaternion(targetMapRightRotation, 0.3f).SetEase(Ease.OutQuad).SetDelay(0.2f);
-            mapHolderRect.DORotateQuaternion(targetMapHolderRotation, 0.3f).SetEase(Ease.OutQuad).SetDelay(0.2f);
             isFolded = false;
+            mapRightPageRect.DORotateQuaternion(targetMapRightRotation, 0.3f).SetEase(Ease.OutQuad).SetDelay(0.2f);
+            mapHolderRect.DORotateQuaternion(targetMapHolderRotation, 0.3f).SetEase(Ease.OutQuad).SetDelay(0.2f)
+                .OnComplete(() => zoomTextHolder.SetActive(true));
         }       
     }
 
@@ -46,9 +71,10 @@ public class MapPanel : MonoBehaviour
     {
         if (!isFolded)
         {
+            isFolded = true;
             mapRightPageRect.DORotateQuaternion(originalMapRightRotation, 0.3f).SetEase(Ease.OutQuad).SetDelay(0.2f);
             mapHolderRect.DORotateQuaternion(originalMapHolderRotation, 0.3f).SetEase(Ease.OutQuad).SetDelay(0.2f);
-            isFolded = true;
+            zoomTextHolder.SetActive(false);
         }        
     }
 
@@ -62,4 +88,21 @@ public class MapPanel : MonoBehaviour
         mapHolderRect.DOAnchorPos(originalMapHolderPosition, 0.3f).SetEase(Ease.OutQuad).SetDelay(0.2f);
     }
 
+    public void ToggleMapZoom()
+    {
+        if (isZoomedIn)
+        {
+            mapLeftImage.sprite = worldMapLeftSprite;
+            mapRightImage.sprite = worldMapRightSprite;
+            zoomText.text = "Zoom In";
+        }
+        else
+        {
+            mapLeftImage.sprite = roomMapLeftSprite;
+            mapRightImage.sprite = roomMapRightSprite;
+            zoomText.text = "Zoom Out";
+        }
+
+        isZoomedIn = !isZoomedIn;
+    }
 }
