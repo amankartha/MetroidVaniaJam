b4df1bb384251fb6b3f15a3bda786899b5869cd0
Takoyyaki <yyinl695662@gmail.com>
2024-03-14 01:34:34 -0400

    area name display ui upon entering different area

--- Assets/Game/Scripts/UI/Joyce/MessageCanvasUI.cs
+++ Assets/Game/Scripts/UI/Joyce/MessageCanvasUI.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
 
 public class MessageCanvasUI : MonoBehaviour
 {
     [SerializeField] RectTransform areaNameDisplayHolderRect;
+    [SerializeField] TMP_Text areaNameText;
     public Vector2 targeAreaNameDisplayHolderPosition;
     Vector2 originalAreaNameDisplayHolderPosition;
 
@@ -30,8 +32,11 @@ public class MessageCanvasUI : MonoBehaviour
 
     public void ShowAreaName()
     {
+        Sequence sequence = DOTween.Sequence();
         areaNameDisplayHolderRect.gameObject.SetActive(true);
-        areaNameDisplayHolderRect.DOAnchorPos(targeAreaNameDisplayHolderPosition, 0.3f).SetEase(Ease.OutQuad).SetDelay(0.1f);
+        sequence.Append(areaNameDisplayHolderRect.DOAnchorPos(targeAreaNameDisplayHolderPosition, 0.3f).SetEase(Ease.OutQuad).SetDelay(0.1f));
+        sequence.AppendInterval(1.5f);
+        sequence.AppendCallback(() => HideAreaName());
     }
 
     public void HideAreaName()
@@ -43,5 +48,10 @@ public class MessageCanvasUI : MonoBehaviour
     void DeactivateAreaNameDisplayHolder()
     {
         areaNameDisplayHolderRect.gameObject.SetActive(false);
+    }
+
+    public void SetAreaNameText(string area)
+    {
+        areaNameText.text = area;
     }
 }
