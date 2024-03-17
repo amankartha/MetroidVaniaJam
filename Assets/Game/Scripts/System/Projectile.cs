80373f9fd346bafceb1f746c39c2dd2b5b916b98
Aman Kartha <amankartha1998@gmail.com>
2024-03-17 02:28:31 -0400

    modified duration and color or particles

--- Assets/Game/Scripts/System/Projectile.cs
+++ Assets/Game/Scripts/System/Projectile.cs
@@ -47,6 +47,6 @@ public class Projectile : MonoBehaviour
         _spriteRenderer.enabled = false;
         _boxCollider2D.enabled = false;
         PS.Play();
-        Destroy(this.gameObject,3f);
+        Destroy(this.gameObject,5f);
     }
 }
