2b352b34f7b1dcca200c08d64acfb5902941e8b3
Aman Kartha <amankartha1998@gmail.com>
2024-03-17 02:31:07 -0400

    pushed fix

--- Assets/Game/Scripts/System/Projectile.cs
+++ Assets/Game/Scripts/System/Projectile.cs
@@ -14,6 +14,7 @@ public class Projectile : MonoBehaviour
     private int _damage;
     private float ArcPower = 5f;
     public ParticleSystem PS;
+    public GameObject trailRenderer;
     protected virtual void Start()
     {
         rb.gravityScale = 0;
@@ -45,6 +46,7 @@ public class Projectile : MonoBehaviour
     public virtual void Explode()
     {
         _spriteRenderer.enabled = false;
+        trailRenderer.SetActive(false);
         _boxCollider2D.enabled = false;
         PS.Play();
         Destroy(this.gameObject,5f);
